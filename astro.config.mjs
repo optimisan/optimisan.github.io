43ce088539106609bdbb9bca215dc46e80c3e85c
Akshat Oke <okeakshat@gmail.com>
2025-02-24 23:01:11 +0530

    More improvements
    Added the expressive code package from astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,15 +8,19 @@ import tailwindcss from '@tailwindcss/vite';
 import { remarkModifiedTime } from './remark-modified-time.mjs';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://optimisan.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [expressiveCode({
+    defaultProps: { wrap: true }
+  }), mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
     remarkPlugins: [remarkModifiedTime, remarkReadingTime],
-  }
+  },
 });
