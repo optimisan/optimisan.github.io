36b00f553b788400bfd0be0d396216d08d2a1050
Akshat Oke <akshaoke@amd.com>
2025-02-24 09:49:05 +0530

    remove about

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -4,7 +4,6 @@ export function remarkModifiedTime() {
     return function (tree, file) {
         const filepath = file.history[0];
         const modifiedTime = execSync(`git log -1 --pretty="format:%cI" -- "${filepath}"`).toString().trim();
-        console.log(`Modified time for ${filepath}: ${modifiedTime}`);
         file.data.astro.frontmatter.lastModified = modifiedTime;
     }
 }
